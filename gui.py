7a701d29534897c0e813804d5520d08b4649d499
RishavMz <58086511+RishavMz@users.noreply.github.com>
2020-09-14 22:54:57 +0530

    Added feature to add all images to docx in GUI

--- gui.py
+++ gui.py
@@ -7,29 +7,100 @@
 
 import tkinter
 from tkinter import *
+from docx2pdf import convert
+from docx import Document
+from docx.shared import Inches, Cm
+import cv2
+import os
+import inspect
+from docx2pdf import convert
 
+images_name = list()
 def convert1():
-    print("Converted to docx / PDF format ")
-def convertfn():
-    convert = tkinter.Tk() 
-    convert.title("Images to PDF / docx format")
-    w = Canvas(convert, width=40, height=60) 
-    w.pack() 
+    converted = tkinter.Tk() 
+    converted.title("Images to docx format completed")
+    w1 = Canvas(converted, width=40, height=60) 
+    w1.pack() 
     canvas_height=20
     canvas_width=200
+    w1.create_text(0, 30, anchor=W, font="Purisa",
+            text="PDF", fill="maroon")
+    print("Converted to docx  format ")
+    document = Document()
+    size = list()
+    imagename = list()
+    for filename in images_name:
+        img = cv2.imread(os.path.join('',filename))
+        if img is not None:
+            print("Found ", filename)
+            imagename.append(filename)
+            image = img.shape
+            width = img.shape[0]
+            height = img.shape[1]
+            size.append([width,height])
+    print('\n', len(imagename), 'images found')  
+    if(len(imagename) == 0):
+        print("\n No image present in folder")
+        label1 = Label(converted,text = "No images found in Images folder!!! \nCreated a blank docx file\n")
+        label1.pack()
+    name = "sample"
+    name = name+'.docx'
+    print(name)
+    for data in range(len(imagename)):
+        print("Processing ",imagename[data])
+        p = document.add_paragraph()
+        r = p.add_run()
+        #print(size[data])
+        if (size[data][0] / size[data][1] >=1.5):
+            #print("Type1")
+            r.add_picture(str(imagename[data]), height = Cm(27) , width = Cm(27/size[data][0]*size[data][1]))    
+        elif (size[data][0] / size[data][1] >=1 ):
+            #print("Type2")
+            r.add_picture(str(imagename[data]), height = Cm(24) , width = Cm(24/size[data][0]*size[data][1]))    
+        else:
+            #print("Type3")
+            r.add_picture(str(imagename[data]), width = Cm(21.4) , height = Cm(21.4/size[data][1]*size[data][0]))
+
+    widmargin = 0.1
+    lenmargin = 0.5    
+    sections = document.sections
+        
+        # Page margins narrowed
+    for section in sections:
+        section.top_margin = Cm(lenmargin)
+        section.bottom_margin = Cm(lenmargin)
+        section.left_margin = Cm(widmargin)
+        section.right_margin = Cm(widmargin)  
+    document.save(name)
+    print('\n==========\n=  Done  =\n==========\n')
+    print("Successfully created a docx file\n")
+    label1 = Label(converted,text = "Successfully created a docx file\n")
+    if(len(imagename)>0):
+        label1.pack()
+    converted.mainloop()
+
+
+def convertfn():
+    convert = tkinter.Tk() 
+    convert.title("Images to docx format")
+    w2 = Canvas(convert, width=40, height=60) 
+    w2.pack() 
+    canvas_height=20
+    canvas_width=200
+    w2.create_text(0, 30, anchor=W, font="Purisa",
+            text="PDF", fill="maroon")
     label1 = Label(convert,text = "Please make sure that you have added all photos to be inserted in pdf serially in the images folder\n\n")
     label1.pack()
-    name = Label(convert,text = "Please enter a name for your docx / PDF file\n")
+    name = Label(convert,text = "Please note, the name of docx/PDF file would be sample.docx/sample.pdf by default.\n You can rename it later\n")
     name.pack()
-    nameentry = Entry(convert)
-    nameentry.pack()
     space = Label(convert,text = "\n")
     space.pack()
-    button1 = tkinter.Button(convert, text='Convert to PDF/docx', width=35, command = convert1)
+    button1 = tkinter.Button(convert, text='Convert to docx', width=35, command = convert1)
     button1.pack()
     space = Label(convert,text = "\n")
     space.pack()
     convert.mainloop()
+
 
 def createpdf1():
     print("Converted from docx to pdf format")
