3ff50bb78cfaf03d2d85d0aa40ed6cd46dca414e
Rishav Mazumdar <rishav.mazumdar123@gmail.com>
2021-07-15 09:45:28 +0530

    Fetching image pathname from sqlite database

--- utils/image2docx.py
+++ utils/image2docx.py
@@ -1,54 +1,76 @@
 from docx import Document
-from docx.shared import Inches, Cm
-import cv2
+from docx.shared import  Cm
+from cv2 import imread
 import os, inspect
+import sqlite3
 
 
+# Initializing variables
+imageselected = list()
+imagedimension = list()                   
+imagepath = list()   
 document = Document()
-size = list()                   # Stores Image dimensions (for resizing into document)
-imagename = list()              # Stores Images pathname which are to be added to docx file
 
+
+# Initializing location and path 
 path= os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-
+dbpath = path+'\..\\temp\database\Image2PDF.db'
 imgpath = path+'\..\\temp\images'
+docxpath = path + '\..\\temp\doc\sample.docx' 
 
-for filename in os.listdir(imgpath):   # Storing data for indivisual images
-    filepath = os.path.join(imgpath,filename)
-    img = cv2.imread(filepath)
+
+# Fetching data from sqlite database
+dbconn = sqlite3.connect(dbpath)
+querydata = dbconn.execute("SELECT * FROM IMAGES;")
+for row in querydata:
+    imageselected.append(row[2])
+
+
+# Collecting data about individual images
+for filepath in imageselected:            
+    img = imread(filepath)
     if img is not None:
         print("Found ", filepath)
-        imagename.append(filepath)
+        imagepath.append(filepath)
         image = img.shape
         width = img.shape[0]
         height = img.shape[1]
-        size.append([width,height])
+        imagedimension.append([width,height])
 
-print('\n', len(imagename), 'images found')  
-if(len(imagename) == 0):
+
+print('\n', len(imagepath), 'images found')  
+if(len(imagepath) == 0):
     print("\n No image present in folder")
     quit(0)          
-name = path + '\..\\temp\doc\sample.docx'      
 
-for data in range(len(imagename)):      # Adding all images to docx file (along with proper resizing)
-    print("Processing ",imagename[data])
+     
+# Resizing images and adding them to docx file
+for data in range(len(imagepath)):      
+    print("Processing ",imagepath[data])
     p = document.add_paragraph()
     r = p.add_run()
-    if (size[data][1] / size[data][0] >= 1):
-        r.add_picture(str(imagename[data]), height = Cm((20/size[data][1]) * size[data][0]) , width = Cm(20)  )  
-    elif(size[data][0] / size[data][1] >= (25.94/20.59) ):
-        r.add_picture(str(imagename[data]), height = Cm(25) , width = Cm(25/size[data][0]*size[data][1]))    
+    if (imagedimension[data][1] / imagedimension[data][0] >= 1):
+        r.add_picture(str(imagepath[data]), height = Cm((20/imagedimension[data][1]) * imagedimension[data][0]) , width = Cm(20)  )  
+    elif(imagedimension[data][0] / imagedimension[data][1] >= (25.94/20.59) ):
+        r.add_picture(str(imagepath[data]), height = Cm(25) , width = Cm(25/imagedimension[data][0]*imagedimension[data][1]))    
     else:
-        r.add_picture(str(imagename[data]), width = Cm(20) , height = Cm(25))
+        r.add_picture(str(imagepath[data]), width = Cm(20) , height = Cm(25))
+
+                   
+# Handling page margins for docx file
 widmargin = 0.5
 lenmargintop = 1.5
 lenmarginbottom = 0.5    
 sections = document.sections
-                   
-for section in sections:                # Page margins narrowed
+
+
+for section in sections:                
     section.top_margin = Cm(lenmargintop)
     section.bottom_margin = Cm(lenmarginbottom)
     section.left_margin = Cm(widmargin)
     section.right_margin = Cm(widmargin)  
-document.save(name)
+document.save(docxpath)
+
+
 print('\n==========\n=  Done  =\n==========\n')
 print("Successfully created a docx file\n")
